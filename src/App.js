0a24637a75bdd557104e8734f17fe4eb6437fdf6
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-05-04 23:07:55 -0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import './App.css';
 import { ButtonWrapper, UseState, Wrapper } from './components/UseState/UseState';
+import React, { useState } from 'react';
 
 
 function App() {
+
+  const [count, setCount] = useState(4)
+   
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1)
+  }
+  function increaseCount(){
+    setCount(prevCount => prevCount + 1)
+  }
   
   return (
     <Wrapper>
       <UseState />
 
-      <ButtonWrapper>-</ButtonWrapper>
-      <span>0</span>
-      <ButtonWrapper>+</ButtonWrapper>
+      <ButtonWrapper onClick={decrementCount}>-</ButtonWrapper>
+      <span>{count}</span>
+      <ButtonWrapper onClick={increaseCount}>+</ButtonWrapper>
     </Wrapper>
 
   );
